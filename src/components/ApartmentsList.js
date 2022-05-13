dad7d3d89e4aa9689a57291bbd33317a2a16ac14
didicozaur <moisin.diana@yahoo.com>
2022-05-13 16:42:18 +0200

    added ApartmentsList component and slyles to it

---
+++ src/components/ApartmentsList.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "./ApartmentsList.css";
+
+function ApartmentsList() {
+  const [apartments, setApartments] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://ironbnb-m3.herokuapp.com/apartments")
+      //.get(process.env.REACT_APP_API_URL + "/apartments") is not working
+      .then((response) => {
+        setApartments(response.data);
+      })
+      .catch((e) => console.log("error getting apartments from API", e));
+  }, []);
+
+  return (
+    <section className="ApartmentsList">
+      <h1>ApartmentsList</h1>
+
+      {apartments.map((element) => {
+        return (
+          <div className="apartment-summary">
+            <h3>{element.title}</h3>
+            <img src={element.img} alt={element.title} />
+            Price: {element.pricePerDay} <br />
+            <button>More details</button>
+          </div>
+        );
+      })}
+    </section>
+  );
+}
+
+export default ApartmentsList;
