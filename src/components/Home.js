31496d3a5d06b144d06e11ebf309862a7f058771
didicozaur <moisin.diana@yahoo.com>
2022-05-13 16:41:18 +0200

    added the homepage

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import apartments from "../assets/apartments.jpg";
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome</h1>
+      <img src={apartments.jpg} alt="apartments" />
+    </div>
+  );
+}
+
+export default Home;
